4047b1731c6b0fa4bb3a4c8916958d9f28133d37
Leksa <admin@petiteweb.ru>
2022-09-17 21:35:56 +0300

    js part 2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const getNumber = (max = 11, min = 0) => Math.floor(Math.random() * (max - min) + min);
+const optimizeNumber = (n) => (n + "").length === 2 ? n : "0" + n;
 
 const data = [];
 let n = getNumber(41, 1);
@@ -8,3 +9,9 @@ while(n--) {
 }
 
 console.log(data);
+
+const box = document.querySelector(".box");
+
+for (let val of data) {
+    box.innerHTML += `<div class="card">${optimizeNumber(val)}</div>`;
+}
